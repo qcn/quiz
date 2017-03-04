7fa1d4ad89802e8ef400131d65273ffd38ee4cdc
qcn <github.qcn@ryux.net>
2017-03-04 21:28:01 +1100

    updating models

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db import forms
 
 # Create your models here.
 
@@ -7,6 +8,9 @@ MAX_TITLE_LENGTH = 50
 MAX_QUESTION_LENGTH = 1000
 MAX_MC_ANSWER_LENGTH = 500
 MAX_SHORT_TEXT_LENGTH = 30
+
+
+VALIDATE_MC_TEXT = "Multiple choice question requires a correct answer"
 
 
 class Quiz(models.Model):
@@ -19,6 +23,8 @@ class Question(models.Model):
 
     class Meta:
         abstract = True # wrapper for an actual question
+        # assume questions are added in order
+        ordering = ['id']
 
 
 # multiple choice question
@@ -27,6 +33,18 @@ class MCQuestion(Question):
     # if order of answers doesn't matter, we can randomise
     randomise_answers = models.BooleanField(default=False)
 
+    # get answers either in order or in random order
+    def get_answers(self):
+        if (self.randomise_answers):
+            return self.mcanswer_set.order_by('?')
+        return self.mcanswer_set.all()
+
+    # make sure a multiple choice question has at least one correct answer
+    def clean(self):
+        super(MCQuestion, self).clean()
+        if not self.mcanswer_set.filter(correct=True):
+            raise forms.ValidationError(VALIDATE_MC_TEXT)
+        
 
 # short answer question
 class SAQuestion(Question):
@@ -40,6 +58,11 @@ class MCAnswer(models.Model):
     text = models.CharField(max_length=MAX_MC_ANSWER_LENGTH)
     # short text for analysis
     short_text = models.CharField(max_length=MAX_SHORT_TEXT_LENGTH)
-    # store correctness here in case future quizzes have multiple
+    # store correctness here in case questions have multiple
     # correct answers
     correct = models.BooleanField()
+
+    class Meta:
+        # for now, assume answers are added in order. Add an ordering
+        # field if this isn't the case.
+        ordering = ['id']
