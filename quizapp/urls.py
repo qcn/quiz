7c440ab2b2cdeb73b494cf5583686fef6bb56d9d
qcn <github.qcn@ryux.net>
2017-03-05 01:35:31 +1100

    Basic taking-of-quiz

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import *
 
+app_name = "quizapp"
 urlpatterns = [
-    url(r'^take/(?P<quizid>[0-9]+)$', take_quiz, name="quizapp-take"),
+    url(r'^take/(?P<quizid>[0-9]+)$', take_quiz, name="take"),
+    url(r'^take/(?P<quizid>[0-9]+)/submit/$', submit_quiz, name="submit"),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.forms import formset_factory
 from .forms import MCForm, SAForm, BaseMCFormSet
-from .models import Quiz
+from .models import Quiz, QuizAnswerSet
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
@@ -26,4 +27,41 @@ def take_quiz(request, **kwargs):
     saqns = qz.get_sa_questions()
     sa_forms = [SAForm(qn, prefix="sa"+str(qn.pk)) for qn in saqns]
     qn_forms = mc_forms + sa_forms
-    return render(request, 'take_quiz.html', {'qn_forms': qn_forms})
+    return render(request, 'take_quiz.html',
+                           {
+                            'qn_forms': qn_forms,
+                            'qz': qz,
+                           })
+
+
+def submit_quiz(request, **kwargs):
+    qz = get_object_or_404(Quiz, pk=kwargs['quizid'])
+    # Construct a new QuizAnswerSet from the form data
+    if request.method == 'POST':
+        # iterate through each of the questions
+        # create and assign a dictionary
+        answer_dict = {}
+        for key in request.POST:
+            if key[:2] == "mc": # multiple choice prefix
+                # cut off the prefix and suffix
+                qn_id = int(key[2:-8])
+                answer_dict[qn_id] = [int(x) for x in request.POST[key]]
+            elif key[:2] == "sa": # short answer prefix
+                # cut off the prefix and suffix
+                qn_id = int(key[2:-8])
+                answer_dict[qn_id] = request.POST[key]
+        # we don't care about anything else like CSRF tokens
+
+        # save new QuizAnswerSet
+        new_answers = QuizAnswerSet(qz=qz, answers=answer_dict)
+        new_answers.save()
+
+        # redirect to confirmation page
+        return render(request, 'quiz_submitted.html',
+                        {
+                            'qz': qz,
+                        })
+
+    # this page should only be accessed by a submission
+    return HttpResponseForbidden(
+        "You do not have permission to access this page")
